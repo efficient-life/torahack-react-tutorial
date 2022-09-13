db72224c805fdc0aa67f45f78584ca8f1e22ab5e
鈴山佳宏 <suzusanyoshihiro@admin.local>
2022-09-13 16:27:57 +0900

    useEffect/add

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,27 @@ import "./App.css";
 import Counter from "./components/Counter";
 import ToggleButton from "./components/ToggleButton";
 import TextInput from "./components/TextInput";
+import { useEffect, useState } from "react";
 
 function App() {
+  // const [name, setName] = useState("");
+  // const [id, setId] = useState("efficient-life");
+  // const ids = ["efficient-life", "aws", "google", "facebook"];
+  // const getRandomID = () => {
+  //   const _id = ids[Math.floor(Math.random() * ids.length)];
+  //   setId(_id);
+  // };
+  // useEffect(() => {
+  //   fetch(`https://api.github.com/users/${id}`);
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //       setName(data.name);
+  //     })
+  //     .catch((error) => {
+  //       console.log("error");
+  //     });
+  // }, [id]);
   return (
     <div className="App">
       {/* <header className="App-header">
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
@@ -8,6 +8,17 @@ const Counter = () => {
   const countDown = () => {
     setCount((prevState) => prevState - 1);
   };
+  useEffect(() => {
+    console.log("Countレンダリング", count);
+  }, []);
+  // 引数に渡したcountを変更したときに発火
+  useEffect(() => {
+    console.log("Countレンダリング", count);
+  }, [count]);
+  // triger1とtriger2が変更された場合発火
+  //   useEffect(() => {
+  //     console.log("Countレンダリング", count);
+  //   }, [trigger1, trigger2]);
   return (
     <div>
       <p>現在のカウント数{count}</p>
